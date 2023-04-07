e9bf7cf1257ae60d47902eba45832b112efabf23
francesca sandford <fran6san@gmail.com>
2023-04-07 14:42:33 +0100

    further set-up

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module MercedesChallenge
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.assets.precompile += %w[*.css *.scss]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
