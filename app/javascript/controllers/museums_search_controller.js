92c304b95a6b81e2ca0a8eac6dc5c956bc61d3b8
francesca sandford <fran6san@gmail.com>
2023-07-13 13:17:59 +0100

    keep it DRY and add code to catch fetch errors

--- app/javascript/controllers/museums_search_controller.js
+++ app/javascript/controllers/museums_search_controller.js
@@ -6,6 +6,9 @@ export default class extends Controller {
   static values = { api: String };
 
   submit(event) {
+    event.preventDefault();
+    this.resultsTarget.innerHTML = '';
+
     if (this.longitudeTarget.value === '' | this.latitudeTarget.value === '') {
       this.unsuccessful(event)
     } else {
@@ -14,17 +17,27 @@ export default class extends Controller {
   }
 
   async obtainMuseums(longitude, latitude) {
-    const response = await fetch(`https://api.mapbox.com/search/searchbox/v1/category/museum?access_token=${this.apiValue}&limit=10&proximity=${longitude},${latitude}`);
-    const data = await response.json();
-
-    for (const museum of data.features) {
-      let postCode = museum.properties.context.postcode.name;
-
-      if (Object.keys(museumsObj).includes(postCode) === false) {
-        museumsObj[postCode] = [museum.properties.name];
-      } else {
-        museumsObj[postCode].push(museum.properties.name);
+    try {
+      const response = await fetch(`https://api.mapbox.com/search/searchbox/v1/category/museum?access_token=${this.apiValue}&limit=10&proximity=${longitude},${latitude}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
       }
+      const data = await response.json();
+
+      for (const museum of data.features) {
+        let postCode = museum.properties.context.postcode.name;
+
+        if (Object.keys(museumsObj).includes(postCode) === false) {
+          museumsObj[postCode] = [museum.properties.name];
+        } else {
+          museumsObj[postCode].push(museum.properties.name);
+        }
+      }
+    } catch (error) {
+      console.error(`Could not get museums: ${error}`);
+      let para = document.createElement("p");
+      para.textContent = "Apologies, there is an error in obtaining the museums."
+      this.resultsTarget.appendChild(para);
     }
   }
 
@@ -40,17 +53,13 @@ export default class extends Controller {
     this.latitudeTarget.value = '';
   }
 
-  unsuccessful(event) {
-    event.preventDefault();
-    this.resultsTarget.innerHTML = '';
+  unsuccessful() {
     let para = document.createElement("p")
     para.textContent = "You're missing a coordinate, try again.."
     this.resultsTarget.appendChild(para);
   }
 
-  async successful(event) {
-    event.preventDefault();
-    this.resultsTarget.innerHTML = '';
+  async successful() {
     museumsObj = {};
     await this.obtainMuseums(this.longitudeTarget.value, this.latitudeTarget.value);
     this.displayMuseums();
